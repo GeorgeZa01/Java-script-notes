7138d3ff2a369829ccbf12442f35918b2165d570
GeorgeZa01 <jemaile75@gmail.com>
2024-11-11 08:55:21 +0200

    Notes

---
+++ variables.js
@@ -0,0 +1,86 @@
+//String Variables
+// let name = "Jemaile";
+// const name = "Jemaile";
+
+// console.log("My name is " + name );
+
+
+// Number variables
+// let number1 = 10; 
+
+
+//Boolean Variables
+// let value = true; //false
+// console.log(value);
+
+
+//Undefined Variables
+// let myVariable;
+// console.log(myVariable);
+
+
+// Null Variables
+// let myVariable = null;
+// console.log(myVariable);
+
+
+//Symbol Variables
+// let mySymbol = Symbol();
+// console.log(mySymbol);
+
+
+
+// let name =true
+// let num = 5
+// console.log(`My name is ${name}`)
+// console.log("My number is /n" + name + num)
+
+//Arrays
+// let myArray = ["anything", 2, true, "string"]
+// console.log(myArray[3]) //Indexing
+
+//CamelCase --> myArray (always use in JS)
+//PascalCase --> MyArray
+//SnakeCase --> my_array
+//KebabCase --> my-array (only use inn vue)
+
+
+//Object literal --> Objects declare (make) an object
+// let table = {
+//     material: "wood",
+//     no_of_legs: 4,
+//     occupied: true
+// }
+// console.log(table["material"]) //--> bracket notation
+// console.log(table.material) //--> dot notation
+// let {material} = table //(wood) // -->destructuring
+// console.log(material)
+
+// () --> parenthenses
+// [] --> brackets
+// {} --> curly braces
+
+
+//Functions
+// function greeting(a,b){
+//     // a & b are parameters
+//     console.log("Hey there little boy!" + (a + b))
+// }
+// // 3 & 4 are arguments 
+// greeting(3,4)
+
+
+//Arithmetic Operators
+// +, -, *, /, %
+// let num1 = 5
+// // ++num1 --> pre increment  
+// // num1++ --> post increment
+// // --num1 --> pre decrement
+// // num1-- --> post decrement
+// console.log(num1++)
+// console.log(num1)
+
+
+//Comparrison Operators
+// ==(strictly equal), ===(loosely equal), !=, !==, >, <, >=, <=
+
